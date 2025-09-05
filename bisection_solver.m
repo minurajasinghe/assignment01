a7d6742a6b8893f31953c84f1378a24d24472ae1
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-09-05 15:11:53 -0400

    Update bisection_solver.m
    Adding Comments to Bisection Solver

--- bisection_solver.m
+++ bisection_solver.m
@@ -1,10 +1,19 @@
 function x = bisection_solver(fun,x_left,x_right)
+    %Create bisection
     mid = (x_right + x_left)/2;
+    
+    %Check if bisection is within tolerance of 0
     if (fun(mid) < 0.001) && (fun(mid) > -0.001)
         x = mid;
+
+    %If left and mid are new points (opposite signs)
     elseif (((fun(x_left))> 0)&&(fun(mid) < 0))||(((fun(x_left) < 0)&&(fun(mid) > 0)))
+        %Call bisection_solver recursively
         x = bisection_solver(fun, x_left, mid);
+    % If mid and right are new points (opposite signs)
+
     elseif (((fun(mid))> 0)&&(fun(x_right) < 0))||(((fun(mid) < 0)&&(fun(x_right) > 0)))
+        %Call bisection_solver recursively
         x = bisection_solver(fun, mid, x_right);
     end
 end
