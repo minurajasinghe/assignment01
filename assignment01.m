657cf5386804c72142c3c56b457440ae8d8b81dc
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-09-07 22:21:23 -0400

    Creating global variables

--- assignment01.m
+++ assignment01.m
@@ -2,6 +2,10 @@
 test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
 test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
 
+%Create Global Variables
+global bisect_guess_list;
+global newton_guess_list;
+global secant_guess_list;
 
 
 %% Plot Function
@@ -35,3 +39,5 @@ root_secant = secant_solver(fun{1},x_1,x_2);
 
 %% Error
 
+
+bisect_error_list = guess_list - root_bisect
