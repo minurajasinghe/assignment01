52ebeeb3ce32dea02a8c2765add759dc01886daa
Amir.O <157446203+mex-turbo-snail@users.noreply.github.com>
2025-09-07 22:59:02 -0400

    Update secant_solver.m
    Update to secant solver w/ global variable

--- secant_solver.m
+++ secant_solver.m
@@ -1,8 +1,11 @@
 function x = secant_solver(fun,x0, x1)
+    global secant_guess_list;
+
     x_n = x1 - fun(x1)*((x1 - x0)/(fun(x1)-fun(x0)));
-    if (fun(x1) < 0.0000000001) && (fun(x1) > -0.00000000001)
+    if (fun(x1) < 10^-14) && (fun(x1) > -10^-14)
         x = x1;
     else 
+        secant_guess_list(end+1) = x1;
         x = secant_solver(fun,x1,x_n);
     end
 end
