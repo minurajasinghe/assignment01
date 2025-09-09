a8c83c25498937cbbd13b07c66f28333d6f95014
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-09-09 17:56:26 -0400

    Create assignment01_3.m

---
+++ assignment01_3.m
@@ -0,0 +1,16 @@
+%EDIT THIS:
+solver_flag = 1;
+x_guess0 = 0.75;
+
+%DO NOT EDIT THIS
+fun = test_function02();
+num_iter = 1000;
+guesslist1 = linspace(-6,-2,num_iter);
+guesslist2 = linspace(6,2,num_iter);
+
+%Filter Parameters
+filter_max = 1e-1;
+filter_min = 1e-14;
+filter_list = [filter_min, filter_max];
+
+convergence_analysis(solver_flag, fun, x_guess0, guesslist1, guesslist2, filter_list)
