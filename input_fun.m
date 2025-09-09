a3a4624d96b26a6d7e2d00c7e696d0aa0f54c26e
minurajasinghe <157316121+minurajasinghe@users.noreply.github.com>
2025-09-09 16:30:32 -0400

    Change input_fun.m and intial work refactoring everything

--- input_fun.m
+++ input_fun.m
@@ -1,7 +1,5 @@
-function fun = input_fun()
+function [f,dfdx] = input_fun(x)
     % Hardcode function and it's derivative into a cell array with anonymous function
-
-    func = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
-    dfdx = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
-    fun = {func, dfdx};
+    f = (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
+    dfdx = 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
 end
