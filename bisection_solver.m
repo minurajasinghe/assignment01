54a6ef9a14eede83141ef04f5a411242aab5ea1e
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-09-10 19:28:52 -0400

    Bisection Secant working w/ new thing

--- bisection_solver.m
+++ bisection_solver.m
@@ -4,7 +4,7 @@ function x = bisection_solver(fun,x_left,x_right)
 
     %Create bisection
     mid = (x_right + x_left)/2;
-    
+   
     %Check if bisection is within tolerance of 0
     if (fun(mid) < 1e-14) && (fun(mid) > -1e-14)
         x = mid;
